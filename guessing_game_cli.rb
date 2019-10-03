19d56bc11692276043b35a423c0d5bd9419897a4
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-10-03 20:38:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,7 @@
 def run_guessing_game
   game_num = rand(1..6)
-  user_num = gets
+  p game_num
+  user_num = gets.chomp
   if user_num == game_num
     puts "You guessed the correct number!"
   elsif user_num != game_num && user_num != "exit"

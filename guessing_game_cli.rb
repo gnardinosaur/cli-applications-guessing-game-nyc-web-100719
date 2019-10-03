a43c7cb4decbf5058293afe7050526769294e035
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-10-03 20:45:37 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   game_num = rand(7) + 1
-  #puts "Pick a number between 1 and 6."
+  puts "Pick a number between 1 and 6."
   user_num = gets.chomp
   if user_num == game_num
     puts "You guessed the correct number!"

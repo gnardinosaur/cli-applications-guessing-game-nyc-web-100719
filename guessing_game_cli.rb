41ac75448a5fed73b263d95800f001fe86fb0458
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-10-03 20:46:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,12 +1,14 @@
-def run_guessing_game
-  game_num = (rand(6) + 1)
+#def run_guessing_game
+  #game_num = rand(6) + 1
   #puts "Pick a number between 1 and 6."
-  user_num = gets.chomp
-  if user_num == game_num
-    puts "You guessed the correct number!"
-  elsif user_num != game_num && user_num != "exit"
-    puts "Sorry! The computer guessed #{game_num}."
-  else user_num == "exit"
-    puts "Goodbye!"
-  end
-end
+#  user_num = gets.chomp
+#  if user_num == game_num
+   # puts "You guessed the correct number!"
+  #elsif user_num != game_num && user_num != "exit"
+  #  puts "Sorry! The computer guessed #{game_num}."
+ # else user_num == "exit"
+#    puts "Goodbye!"
+ # end
+#end
+
+input = gets 
